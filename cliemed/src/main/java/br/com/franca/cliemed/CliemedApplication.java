e910bb747dfba6412e5336b4ba0a1e002b3873f9
Tiago França <tiagofranca.rita@gmail.com>
2024-06-10 14:15:13 -0300

    create project initial commit

---
+++ cliemed/src/main/java/br/com/franca/cliemed/CliemedApplication.java
@@ -0,0 +1,13 @@
+package br.com.franca.cliemed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliemedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CliemedApplication.class, args);
+    }
+
+}
